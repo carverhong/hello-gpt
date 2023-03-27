aa7292628c2ecf1a9c73dcd6513c36416239a62c
carverhong <80571662+carverhong@users.noreply.github.com>
2023-03-27 19:14:09 +0800

    devServe支持发送请求

---
+++ src/App2.vue
@@ -0,0 +1,133 @@
+<template>
+  <div id="app">
+    <div id="input-area">
+      <p v-for="(item, idx) in historyInput" :key="idx">
+        {{ item }}
+      </p>
+      <el-input v-model="currentInput" placeholder="请输入prompt">
+        <template slot="append">
+          <el-button @click="generate">生成组件</el-button>
+        </template>
+      </el-input>
+      <div style="margin-top: 15px">
+        <el-input v-model="prompt" placeholder="请输入测试prompt">
+          <template slot="append">
+            <el-button @click="onSubmit">测试</el-button>
+          </template>
+        </el-input>
+      </div>
+    </div>
+    <component
+      :is="compName"
+      :initTableData="helloTableParams.data"
+      :tableHeader="helloTableParams.columns"
+      :canEdit="helloTableParams.canEdit"
+      :canDelete="helloTableParams.canDelete"
+    />
+
+    <!-- 初始化apiKey -->
+    <el-dialog
+      title="初始化AI助手"
+      :visible.sync="apiKeyDialogShow"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :show-close="false"
+    >
+      <el-input v-model="apiKey" placeholder="请输入ApiKey"></el-input>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="initOpenAI">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import HelloTable from "./components/HelloTable.vue";
+import { generatePrompt } from "./utils/datasource";
+import axios from "axios";
+
+export default {
+  name: "app",
+  components: {
+    HelloTable,
+  },
+  data() {
+    return {
+      historyInput: [],
+      currentInput: "",
+      apiKey: "",
+      apiKeyDialogShow: true,
+      prompt: "",
+      compDSL: "",
+      compName: "",
+      helloTableParams: {},
+    };
+  },
+  methods: {
+    /** 检查apiKey */
+    initOpenAI() {
+      if (this.apiKey) {
+        axios.post("/initOpenAI", { apiKey: this.apiKey }).then((res) => {
+          if (res.data.status === 0) {
+            this.apiKeyDialogShow = false;
+          }
+        });
+      }
+    },
+    generate(params) {
+      // TODO: 调用AI接口生成
+      const dsl = {
+        comp: "HelloTable",
+        params: {
+          tableHeader: ["name", "age", "gender", "address"],
+          tableData: [
+            { name: "John", age: 18, gender: "Male", address: "New York" },
+            { name: "Jane", age: 22, gender: "Female", address: "London" },
+            { name: "Bob", age: 32, gender: "Male", address: "Paris" },
+            { name: "Tom", age: 24, gender: "Male", address: "Tokyo" },
+          ],
+          canEdit: true,
+          canDelete: true,
+        },
+      };
+
+      this.historyInput.push(this.currentInput);
+      this.currentInput = "";
+      this.compName = dsl.comp;
+      if (dsl.comp === "HelloTable") {
+        // this.helloTableParams = dsl.params;
+        this.helloTableParams = params;
+      }
+    },
+    async onSubmit(event) {
+      event.preventDefault();
+      const res = await axios.post("/createCompletion", {
+        prompt: generatePrompt(this.prompt),
+      });
+      if (res.data.status === 0) {
+        const params = eval(res.data.completion.choices[0].text);
+
+        console.log(params);
+        this.generate(params[0].value);
+      } else {
+        console.log(res.err);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#input-area {
+  margin-bottom: 60px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,43 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const { Configuration, OpenAIApi } = require("openai");
+const bodyParser = require('body-parser');
+
+// openAI实例
+let openAI = undefined;
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'https://eolink.o.apispace.com/chatgpt-turbo'
+    onBeforeSetupMiddleware(devServer) {
+      devServer.app.use(bodyParser.json());
+
+      // 初始化openAI
+      devServer.app.post('/initOpenAI', (req, res) => {
+        console.log('=========> /initOpenAI');
+        const configuration = new Configuration({
+          apiKey: req.body.apiKey,
+        });
+        openAI = new OpenAIApi(configuration);
+        res.json({ status: 0 });
+        console.log('<========= /initOpenAI');
+      });
+
+      // 调用createCompletion
+      devServer.app.post('/createCompletion', (req, res) => {
+        console.log('=========> /createCompletion');
+        openAI.createCompletion({
+          model: "text-davinci-003",
+          prompt: req.body.prompt,
+          temperature: 0,
+          max_tokens: 3000,
+        }).then(output => {
+          console.log('<========= /createCompletion', output.data);
+          res.json({ status: 0, completion: output.data });
+        }, err => {
+          console.log('<========= /createCompletion', err);
+          res.json({ status: 1, err: err.message });
+        });
+      });
+    }
   }
-})
+});
