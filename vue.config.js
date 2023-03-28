da18f8bdd198c9e3e44e7a246043da0ec2ce5a70
carverhong <80571662+carverhong@users.noreply.github.com>
2023-03-28 19:28:58 +0800

    支持多实例

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const bodyParser = require('body-parser');
 
 // openAI实例
-let openAI = undefined;
+let openAI = [];
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -17,15 +17,21 @@ module.exports = defineConfig({
         const configuration = new Configuration({
           apiKey: req.body.apiKey,
         });
-        openAI = new OpenAIApi(configuration);
-        res.json({ status: 0 });
+        openAI.push(new OpenAIApi(configuration));
+        res.json({ status: 0, sessionId: openAI.length - 1 });
         console.log('<========= /initOpenAI');
       });
 
       // 调用createCompletion
       devServer.app.post('/createCompletion', (req, res) => {
         console.log('=========> /createCompletion');
-        openAI.createCompletion({
+        const item = openAI[req.body.sessionId];
+        if (!item) {
+          res.json({ status: 1, err: '实例不存在' });
+          return;
+        }
+
+        openAI[req.body.sessionId].createCompletion({
           model: "text-davinci-003",
           prompt: req.body.prompt,
           temperature: 0,
