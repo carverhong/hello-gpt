c930b130b684cc57a7197993116a2b2c370648bc
carverhong <80571662+carverhong@users.noreply.github.com>
2023-04-03 19:31:24 +0800

    调整配置

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   runtimeCompiler: true,
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://127.0.0.1:8999'
+    proxy: {
+      '/initOpenAI': { target: 'http://127.0.0.1:8999' }, // TODO: 替换Asure代理地址
+      '/createChatCompletion': { target: 'http://127.0.0.1:8999' }, // TODO: 替换Asure代理地址
+      '/dataList': { target: 'http://127.0.0.1:9999' } // TODO: 替换后台接口地址
+    },
   }
 });
